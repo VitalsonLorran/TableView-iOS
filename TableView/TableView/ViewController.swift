7d937c8ca3ace5f5ffa2d02743be889dc44a7931
Vitalson Lorran Viana Carvalhal <vitalson_lorran95@ymail.com>
2021-09-21 22:12:23 -0300

    Mexendo um pouco com TableView

--- TableView/TableView/ViewController.swift
+++ TableView/TableView/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Número de secao
+    
+    
+    //Delegate & DataSource
+    @IBOutlet weak var tableView: UITableView!
+    
+    let myArray: Array = ["João", "Maria", "José", "Pedro", "Carlos"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-
+    
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    //numero de linhas por secao
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        //indexPath.row faz um loop da quantidade numerica do array
+        cell.textLabel?.text = myArray[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print("Fez scroll")
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Clicou aqui \(indexPath)")
+    }
+}
